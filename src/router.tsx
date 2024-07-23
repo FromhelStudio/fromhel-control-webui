87c74a90d3e16fc1dc165fd4395bd49035092c1f
Matheus Abrantes <matheus.abrantes@focustextil.com.br>
2024-07-23 11:39:36 -0300

    refactor: removed unused loc

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/sync-in/sync-in';
 import SyncOut from './pages/sync-out/sync-out';
 import Dashboard from './pages/dashboard/dashboard';
-import { useNavigate } from 'react-router-dom';
 
 export default function Router() {
-  const navigate = useNavigate()
   return (
     <Routes>
         <Route path='/login' element={<SyncOut />}/>
