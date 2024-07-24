2ab4ed90dae08e8b441bdb1d5086b320535daed4
matheus <96802002+mtab-dev@users.noreply.github.com>
2024-07-24 08:23:58 -0300

    refactor: sync out form

--- src/pages/sync-out/syncOutForm.ts
+++ src/pages/sync-out/syncOutForm.ts
@@ -1,11 +1,8 @@
 import { z } from 'zod'
 
 export const syncOutSchema = z.object({
-  name: z.optional(z.string().min(1, 'Nome é obrigatório')),
   email: z.optional(z.string().min(1, 'E-mail é obrigatório')),
   password: z.optional(z.string().min(1, 'Senha é obrigatório')),
-  phone: z.optional(z.string().min(1, 'Telefone é obrigatório')),
-  role: z.optional(z.string().min(1, 'Cargo é obrigatório'))
 })
 
 export type TSyncOutSchema = z.infer<typeof syncOutSchema>
