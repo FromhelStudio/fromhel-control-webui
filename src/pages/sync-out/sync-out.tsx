70dfd06f9e24b599da5273e2428fba77a64d9155
mtab-dev <matheus.abrantes@focustextil.com.br>
2024-10-14 11:21:41 -0300

    refactor: handling google login

--- src/pages/sync-out/sync-out.tsx
+++ src/pages/sync-out/sync-out.tsx
@@ -23,6 +23,22 @@ export default function SyncOut(){
   const navigate = useNavigate()
   const { login, googleLogin } = useContext(AuthContext)
 
+  async function handleGoogleLogin(response: any){
+    try{
+      setLogging(true)
+      await googleLogin(response)
+      toast.success('Logado com sucesso')
+      setTimeout(() => {
+        navigate('/dashboard')
+        setLogging(false)
+      }, 1000);
+    }catch(e){
+      toast.error('Falha ao logar com Google')
+      setLogging(false)
+
+    }
+  }
+
   const syncOut = useCallback(async () => {
     setLogging(true)
     const hashedPassword = bcrypt.hashSync(watch('password'), '$2a$10$CwTycUXWue0Thq9StjUM0u')
@@ -85,17 +101,11 @@ export default function SyncOut(){
                               shape='circle'
                               type='icon'
                               onSuccess={async (credentialResponse) => {
-                                  setLogging(true)
-                                  await googleLogin(credentialResponse)
-                                  toast.success('Logado com sucesso')
-                                  setTimeout(() => {
-                                    navigate('/dashboard')
-                                    setLogging(false)
-                                  }, 1000);
+                                 await handleGoogleLogin(credentialResponse)
                               }}
                               onError={() => {
-                                setLogging(false)
                                 toast.error('Falha ao logar com Google');
+                                setLogging(false)
                               }}
                               
                             />;
