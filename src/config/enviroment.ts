77a304541c9f1127ca5ee836070477506b6d890b
mtab-dev <matheus.abrantes@focustextil.com.br>
2024-10-22 07:36:12 -0300

    feat: bullet api env

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -1,5 +1,6 @@
 export const env = {
     BASE_API: process.env.VITE_BASE_API,
+    BULLET_API: process.env.VITE_BULLET_API,
     CLIENT_API: process.env.VITE_CLIENT_API,
     GOOGLE_ID: process.env.VITE_GOOGLE_CLIENT_KEY
 }
--- src/pages/bulletspeel/bulletspeel.tsx
+++ src/pages/bulletspeel/bulletspeel.tsx
@@ -2,11 +2,52 @@ import './bulletspeel.scss'
 import Header from '../../components/header/header'
 import Card from '../../components/card/card'
 import { Download, MessageSquareMore, Star } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import useCookies from '../../hooks/useCookies'
+import axios from 'axios'
+import Loader from '../../components/loader/loader'
+import { env } from '../../config/enviroment'
+import { toast } from 'react-toastify'
+
+interface IFeedbacks {
+    rate: number
+    feedback: string
+}
 
 
 export default function BulletSpeel(){
+    const [feedbacks, setFeedbacks] = useState<IFeedbacks[]>([])
+    const [loader, setLoader] = useState<boolean>(false)
+    const { getCookie } = useCookies()
+    const authTokenName = 'fhs-auth-token'
+
+
+    useEffect(() => {
+        if (getCookie(authTokenName)) {
+          setLoader(true)
+          async function fetchClients() {
+            try {
+              const response = await axios.get(
+                `${env.BULLET_API + '/get'}`
+              )
+              setFeedbacks(response.data)
+              console.log(feedbacks)
+              toast.success('Usuários encontrados!')
+              setLoader(false)
+            } catch (error) {
+              toast.error('Erro ao achar usuários!')
+              console.error('Error fetching clients:', error)
+            }
+            setLoader(false)
+          }
+    
+          fetchClients()
+        }
+      }, [])
+
     return (
         <div className="bullet-page">
+             {loader && <Loader loading={loader} />}
             <Header />
             <div className="ratings-section">
                 <Card 
@@ -16,7 +57,7 @@ export default function BulletSpeel(){
                 />
                 <Card
                 title='Avaliações'
-                data='0'
+                data={feedbacks.length.toString()}
                 />
             </div>
             <div className="ratings-section">
